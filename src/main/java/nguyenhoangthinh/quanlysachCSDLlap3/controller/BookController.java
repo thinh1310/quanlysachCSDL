2cd8d70afbdc104b14641d1e98844118ac9fd43d
thinh1310 <113665001+thinh1310@users.noreply.github.com>
2023-05-25 14:25:50 +0700

    them xong phan add

--- src/main/java/nguyenhoangthinh/quanlysachCSDLlap3/controller/BookController.java
+++ src/main/java/nguyenhoangthinh/quanlysachCSDLlap3/controller/BookController.java
@@ -2,10 +2,13 @@ package nguyenhoangthinh.quanlysachCSDLlap3.controller;
 
 import nguyenhoangthinh.quanlysachCSDLlap3.entity.Book;
 import nguyenhoangthinh.quanlysachCSDLlap3.services.BookService;
+import  nguyenhoangthinh.quanlysachCSDLlap3.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -15,11 +18,26 @@ import java.util.List;
 public class BookController {
     @Autowired
     private BookService bookService;
+    @Autowired
+    private CategoryService categoryService;
 
     @GetMapping
     public String showAllBooks(Model model) {
         List<Book> books = bookService.GetAllBooks();
         model.addAttribute("books", books);
         return "book/list";
+    }
+    @GetMapping("/add")
+    public String addBookForm(Model model)
+    {
+        model.addAttribute("book" ,new Book());
+        model.addAttribute("categories",categoryService.getAllCategories());
+        return "book/add";
+
+    }
+    @PostMapping("/add")
+    public String addBook(@ModelAttribute("book") Book book){
+        bookService.addBook(book);
+        return "redirect:/books";
     }
 }
